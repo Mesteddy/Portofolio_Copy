51dbfb24b2c33f4c78d4942ea9e4757b3a72db11
Tedros Tsegay <tedros.tsegay@bellevuecollege.edu>
2024-07-03 11:52:51 -0700

    Portfolio

---
+++ Portfolio of Tedros Tsegay_files/buttons.js
@@ -0,0 +1,41 @@
+$( "#ncrpbutton" ).attr('data-buttonlink', 'https://ncrp.app')
+
+
+$( "#ncrpbutton" ).click(function() {
+  const buttonlink = $("#ncrpbutton").data('buttonlink')
+
+  window.open(buttonlink)
+});
+
+$( "#ncrpgithubbutton" ).attr('data-buttonlink', 'https://github.com/Negative-Carbon-Reforestation-Project/openlayers-springboot-react-demo')
+
+
+$( "#ncrpgithubbutton" ).click(function() {
+  const buttonlink = $("#ncrpgithubbutton").data('buttonlink')
+
+  window.open(buttonlink)
+});
+
+$( "#linkedin" ).attr('data-buttonlink', 'https://www.linkedin.com/in/tedros-tsegay-8499a4140/')
+
+$( "#linkedin" ).click(function() {
+  const buttonlink = $("#linkedin").data('buttonlink')
+
+  window.open(buttonlink)
+});
+
+$( "#github" ).attr('data-buttonlink', 'https://github.com/ttsega06/2022-GardenMonitoring')
+
+$( "#github" ).click(function() {
+  const buttonlink = $("#github").data('buttonlink')
+
+  window.open(buttonlink)
+});
+
+$( "#resume" ).attr('data-buttonlink', '/Portfolio/resume.html')
+
+$( "#resume" ).click(function() {
+  const buttonlink = $("#resume").data('buttonlink')
+
+  window.open(buttonlink)
+});
---
+++ js/buttons.js
@@ -0,0 +1,45 @@
+$("#ncrpbutton").attr("data-buttonlink", "https://ncrp.app");
+
+$("#ncrpbutton").click(function () {
+  const buttonlink = $("#ncrpbutton").data("buttonlink");
+
+  window.open(buttonlink);
+});
+
+$("#ncrpgithubbutton").attr(
+  "data-buttonlink",
+  "https://github.com/ttsega06/2022-GardenMonitoring"
+);
+
+$("#ncrpgithubbutton").click(function () {
+  const buttonlink = $("#ncrpgithubbutton").data("buttonlink");
+
+  window.open(buttonlink);
+});
+
+$("#linkedin").attr(
+  "data-buttonlink",
+  "https://www.linkedin.com/in/tedros-tsegay-8499a4140"
+);
+
+$("#linkedin").click(function () {
+  const buttonlink = $("#linkedin").data("buttonlink");
+
+  window.open(buttonlink);
+});
+
+$("#github").attr("data-buttonlink", "https://github.com/ttsega06");
+
+$("#github").click(function () {
+  const buttonlink = $("#github").data("buttonlink");
+
+  window.open(buttonlink);
+});
+
+$("#resume").attr("data-buttonlink", "Resume.html");
+
+$("#resume").click(function () {
+  const buttonlink = $("#resume").data("buttonlink");
+
+  window.open(buttonlink);
+});
